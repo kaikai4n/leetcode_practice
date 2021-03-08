e30d27c7e44c9f53ff3a53873f51d80ee0d6010b
kaikai4n <b04902131@ntu.edu.tw>
2021-03-08 13:58:50 +0800

    1376: fix the algo to be faster.

--- problems/1300-1400/1376/2021_03_04.cpp
+++ problems/1300-1400/1376/2021_03_04.cpp
@@ -1,15 +1,24 @@
 /*
  * https://leetcode.com/problems/time-needed-to-inform-all-employees/
- * Runtime: 452 ms, faster than 59.76% of C++ online submissions
- * Memory Usage: 275.8 MB, less than 5.06% of C++ online submissions
+ * Method:
+ *      For each leaf node,
+ *          a. Backtrack its manager to the root or to the manager that has traveled
+ *             and record the backtracking path
+ *          b. Reverse the path to sum the inform time. Note that the started summation
+ *             depends on whether backtracked to the root in step a. Record the inform 
+ *             time in a vector as the traveled manager.
+ * time: O(n), space: O(n)
+ * Runtime: 244 ms, faster than 90.24% of C++ online submissions
+ * Memory Usage: 227.4 MB, less than 5.06% of C++ online submissions
 */
 
 class Solution {
 public:
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
         vector<int> sum(n, -1);
+        int res = 0;
         for (int i = 0; i < n; i ++) {
-            if (sum[i] != -1)
+            if (informTime[i] != 0)  // Only consider the leaf nodes
                 continue;
             int j = manager[i];
             stack<int> s;
@@ -30,10 +39,8 @@ public:
                     sum[s.top()] = sumNow;
             }
             sum[i] = sumNow;
-        }
-        int res = 0;
-        for (int i = 0; i < n; i ++)
             res = max(res, sum[i]);
+        }
         return res;
     }
 };
