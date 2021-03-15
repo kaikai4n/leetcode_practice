070e8a0ab07aeb8cd0b7eb396e60cf0f9cf0d7eb
kaikai4n <b04902131@ntu.edu.tw>
2021-03-15 15:59:41 +0800

    32: optimize the old solution.

--- problems/1-100/32/2021_03_15.cpp
+++ problems/1-100/32/2021_03_15.cpp
@@ -1,14 +1,43 @@
 /*
  * https://leetcode.com/problems/longest-valid-parentheses/
- * Method: Use a stack to record the previous balance.
- * time: O(S), space: O(S)
- * Runtime: 8 ms, faster than 32.53% of C++ online submissions
- * Memory Usage: 7.5 MB, less than 43.50% of C++ online submissions
+ * MethodStack and MethodStackOpt: time and space: O(S)
 */
 
 class Solution {
 public:
-    int longestValidParentheses(string s) {
+    int longestValidParenthesesMethodStackOpt(string s) {
+        /*
+         * Method: An optimized stack solution
+         * time: O(S), space: O(S)
+         * Runtime: 4 ms, faster than 85.45% of C++ online submissions
+         * Memory Usage: 7.2 MB, less than 62.10% of C++ online submissions
+        */
+        int longest = 0;
+        if (s.length() == 0)
+            return 0;
+        stack<int> st;
+        st.push(-1);
+        for (int i = 0; i < s.length(); i ++) {
+            char c = s[i];
+            if (c == '(') {
+                st.push(i);
+            } else {
+                st.pop();
+                if (!st.empty())
+                    longest = max(longest, i - st.top());
+                else
+                    st.push(i);
+            }
+        }
+        return longest;
+    }
+    int longestValidParenthesesMethodStack(string s) {
+        /*
+         * Method: Use a stack to record the previous balance.
+         * time: O(S), space: O(S)
+         * Runtime: 8 ms, faster than 32.53% of C++ online submissions
+         * Memory Usage: 7.5 MB, less than 43.50% of C++ online submissions
+         */
         int balance = 0;
         int longest = 0;
         if (s.length() == 0)
