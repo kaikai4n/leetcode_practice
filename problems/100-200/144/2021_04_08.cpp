b2a4ec80bb1d7bbe1318974a02206c4dc04bac3b
kaikai4n <b04902131@ntu.edu.tw>
2021-04-08 15:55:33 +0800

    144: Morris like traversal.

--- problems/100-200/144/2021_04_08.cpp
+++ problems/100-200/144/2021_04_08.cpp
@@ -1,11 +1,40 @@
 /*
  * https://leetcode.com/problems/binary-tree-preorder-traversal/
- * Runtime: 4 ms, faster than 45.77% of C++ online submissions
- * Memory Usage: 8.4 MB, less than 72.28% of C++ online submissions
  */
 class Solution {
 public:
-    vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> preorderTraversalMorris(TreeNode* root) {
+        /*
+         * Morris like traversal.
+         * time: O(n), space: O(1)
+         * Runtime: 4 ms, faster than 45.77% of C++ online submissions
+         * Memory Usage: 8.3 MB, less than 72.28% of C++ online submissions
+        */
+        vector<int> res;
+        while (root != nullptr) {
+            res.push_back(root->val);
+            if (root->left == nullptr) {
+                root = root->right;
+            } else if (root->right == nullptr) {
+                root = root->left;
+            } else {
+                // both left and right are not null
+                TreeNode* node = root->left;
+                while (node->right != nullptr)
+                    node = node->right;
+                node->right = root->right;
+                root->right = nullptr;
+                root = root->left;
+            }
+        }
+        return res;
+    }
+    vector<int> preorderTraversalRecursive(TreeNode* root) {
+        /*
+         * time: O(n), space: O(n)
+         * Runtime: 4 ms, faster than 45.77% of C++ online submissions
+         * Memory Usage: 8.4 MB, less than 72.28% of C++ online submissions
+        */
         vector<int> res;
         stack<TreeNode*> s;
         s.push(root);
