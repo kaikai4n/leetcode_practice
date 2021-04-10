72745ead8dc96919fd797f7d29db177cbc8e4f71
kaikai4n <b04902131@ntu.edu.tw>
2021-04-10 17:04:57 +0800

    144: Morris preorder algorithm.

--- problems/100-200/144/2021_04_08.cpp
+++ problems/100-200/144/2021_04_08.cpp
@@ -5,10 +5,41 @@ class Solution {
 public:
     vector<int> preorderTraversalMorris(TreeNode* root) {
         /*
-         * Morris like traversal.
+         * Morris Algorithm: time: O(n), space: O(1)
+         * Runtime: 0 ms, faster than 100.00% of C++ online submissions
+         * Memory Usage: 8.4 MB, less than 72.14% of C++ online submissions
+        */
+        vector<int> res;
+        TreeNode *p1 = root, *p2 = nullptr;
+        while (p1 != nullptr) {
+            p2 = p1->left;
+            if (p2 == nullptr) {
+                res.push_back(p1->val);
+                p1 = p1->right;
+            } else {
+                while (p2->right != nullptr && p2->right != p1)
+                    p2 = p2->right;
+                if (p2->right == nullptr) {
+                    p2->right = p1;
+                    res.push_back(p1->val);
+                    p1 = p1->left;
+                } else {
+                    p2->right = nullptr;
+                    p1 = p1->right;
+                }
+            }
+        }
+        return res;
+    }
+    vector<int> preorderTraversalMorrisLike(TreeNode* root) {
+        /*
+         * A Morris like traversal but modified the tree structure.
+         *   1            1
+         * 2   3    =>  2
+         *               3
          * time: O(n), space: O(1)
          * Runtime: 4 ms, faster than 45.77% of C++ online submissions
-         * Memory Usage: 8.3 MB, less than 72.28% of C++ online submissions
+         * Memory Usage: 8.3 MB, less than 72.14% of C++ online submissions
         */
         vector<int> res;
         while (root != nullptr) {
@@ -31,7 +62,6 @@ public:
     }
     vector<int> preorderTraversalRecursive(TreeNode* root) {
         /*
-         * time: O(n), space: O(n)
          * Runtime: 4 ms, faster than 45.77% of C++ online submissions
          * Memory Usage: 8.4 MB, less than 72.28% of C++ online submissions
         */
